cdef80bcd34833dcb53ba99d90279b823eb3f1fb
Murat KIRDAL <muratkirdal@luduarts.com>
2026-01-30 14:53:45 +0300

    feat(mediator): wire sell flow to ShopMediator
    Handle SellClicked event and update views on SellCompleted.

--- Assets/CarBuy/Scripts/ShopMediator.cs
+++ Assets/CarBuy/Scripts/ShopMediator.cs
@@ -17,7 +17,7 @@ namespace CarBuy
         [SerializeField] private ConfirmationPopup m_ConfirmationPopup;
         [SerializeField] private CarouselView m_CarouselView;
         [SerializeField] private CurrencyHudView m_CurrencyHudView;
-        [SerializeField] private PurchasePanelView m_PurchaseView;
+        [SerializeField] private TradePanelView m_TradeView;
         [SerializeField] private StatsPanelView m_StatsView;
 
         [Header("3D Display")]
@@ -32,9 +32,11 @@ namespace CarBuy
         {
             m_CarouselView.VehicleSelected -= HandleVehicleSelected;
             m_StatsView.ColorSelected -= HandleColorSelected;
-            m_PurchaseView.PurchaseClicked -= HandlePurchaseRequest;
+            m_TradeView.BuyClicked -= HandleBuyRequest;
+            m_TradeView.SellClicked -= HandleSellRequest;
             m_CurrencyService.BalanceChanged -= HandleBalanceChanged;
-            m_TransactionService.PurchaseCompleted -= HandlePurchaseCompleted;
+            m_TransactionService.PurchaseCompleted -= HandleBuyCompleted;
+            m_TransactionService.SellCompleted -= HandleSellCompleted;
         }
 
         public void Initialize()
@@ -60,9 +62,11 @@ namespace CarBuy
 
             m_CarouselView.VehicleSelected += HandleVehicleSelected;
             m_StatsView.ColorSelected += HandleColorSelected;
-            m_PurchaseView.PurchaseClicked += HandlePurchaseRequest;
+            m_TradeView.BuyClicked += HandleBuyRequest;
+            m_TradeView.SellClicked += HandleSellRequest;
             m_CurrencyService.BalanceChanged += HandleBalanceChanged;
-            m_TransactionService.PurchaseCompleted += HandlePurchaseCompleted;
+            m_TransactionService.PurchaseCompleted += HandleBuyCompleted;
+            m_TransactionService.SellCompleted += HandleSellCompleted;
         }
 
         private void HandleVehicleSelected(int index, VehicleData vehicle)
@@ -72,8 +76,8 @@ namespace CarBuy
             bool isOwned = m_VehicleService.IsVehicleOwned(vehicle.Id);
 
             m_StatsView.DisplayVehicle(vehicle);
-            m_PurchaseView.DisplayVehicle(vehicle, isOwned);
-            m_PurchaseView.SetPurchaseEnabled(CanAffordCurrentVehicle());
+            m_TradeView.DisplayVehicle(vehicle, isOwned);
+            m_TradeView.SetBuyEnabled(CanAffordCurrentVehicle());
             m_VehicleShowcase.DisplayVehicle(vehicle, m_State.SelectedColorIndex);
         }
 
@@ -85,7 +89,7 @@ namespace CarBuy
 
         private void HandleBalanceChanged(int newBalance)
         {
-            m_PurchaseView.SetPurchaseEnabled(CanAffordCurrentVehicle());
+            m_TradeView.SetBuyEnabled(CanAffordCurrentVehicle());
         }
 
         private bool CanAffordCurrentVehicle()
@@ -94,7 +98,7 @@ namespace CarBuy
             return m_CurrencyService.CurrentBalance >= vehicle.Price;
         }
 
-        private void HandlePurchaseRequest()
+        private void HandleBuyRequest()
         {
             VehicleData vehicle = m_State.CurrentVehicle;
 
@@ -102,25 +106,54 @@ namespace CarBuy
             {
                 if (confirmed)
                 {
-                    ProcessPurchase(vehicle);
+                    ProcessBuy(vehicle);
                 }
             });
         }
 
-        private void ProcessPurchase(VehicleData vehicle)
+        private void ProcessBuy(VehicleData vehicle)
         {
             m_TransactionService.PurchaseVehicle(vehicle.Id, m_State.SelectedColorIndex);
         }
 
-        private void HandlePurchaseCompleted(PurchaseTransaction transaction)
+        private void HandleBuyCompleted(VehicleTransaction transaction)
         {
             m_CarouselView.MarkItemAsOwned(transaction.VehicleId);
 
             VehicleData currentVehicle = m_State.CurrentVehicle;
 
             m_StatsView.DisplayVehicle(currentVehicle);
-            m_PurchaseView.DisplayVehicle(currentVehicle, true);
-            m_PurchaseView.SetPurchaseEnabled(false);
+            m_TradeView.DisplayVehicle(currentVehicle, true);
+            m_TradeView.SetBuyEnabled(false);
+        }
+
+        private void HandleSellRequest()
+        {
+            VehicleData vehicle = m_State.CurrentVehicle;
+
+            m_ConfirmationPopup.ShowSell(vehicle.DisplayName, vehicle.SalePrice, confirmed =>
+            {
+                if (confirmed)
+                {
+                    ProcessSell(vehicle);
+                }
+            });
+        }
+
+        private void ProcessSell(VehicleData vehicle)
+        {
+            m_TransactionService.SellVehicle(vehicle.Id);
+        }
+
+        private void HandleSellCompleted(VehicleTransaction transaction)
+        {
+            m_CarouselView.MarkItemAsNotOwned(transaction.VehicleId);
+
+            VehicleData currentVehicle = m_State.CurrentVehicle;
+
+            m_StatsView.DisplayVehicle(currentVehicle);
+            m_TradeView.DisplayVehicle(currentVehicle, false);
+            m_TradeView.SetBuyEnabled(CanAffordCurrentVehicle());
         }
 
         public void OpenShop()
@@ -131,8 +164,8 @@ namespace CarBuy
             bool isOwned = m_VehicleService.IsVehicleOwned(firstVehicle.Id);
 
             m_StatsView.DisplayVehicle(firstVehicle);
-            m_PurchaseView.DisplayVehicle(firstVehicle, isOwned);
-            m_PurchaseView.SetPurchaseEnabled(CanAffordCurrentVehicle());
+            m_TradeView.DisplayVehicle(firstVehicle, isOwned);
+            m_TradeView.SetBuyEnabled(CanAffordCurrentVehicle());
             m_VehicleShowcase.DisplayVehicle(firstVehicle, 0);
         }
 
