46f4cf6d333e0d8d4cd09176fd0b815da7db4eb7
Murat KIRDAL <muratkirdal@luduarts.com>
2026-01-30 14:53:06 +0300

    refactor(data): replace PurchasePanelConfig with TradePanelConfig
    Add SellColor property to support sell price display styling.

--- Assets/CarBuy/Scripts/Data/Shop/TradePanelConfig.cs
+++ Assets/CarBuy/Scripts/Data/Shop/TradePanelConfig.cs
@@ -2,14 +2,16 @@ using UnityEngine;
 
 namespace CarBuy.Data
 {
-    [CreateAssetMenu(fileName = "PurchasePanelConfig", menuName = "CarBuy/Configs/Purchase Panel")]
-    public class PurchasePanelConfig : ScriptableObject
+    [CreateAssetMenu(fileName = "TradePanelConfig", menuName = "CarBuy/Configs/Trade Panel")]
+    public class TradePanelConfig : ScriptableObject
     {
         [Header("Price Colors")]
         [SerializeField] private Color m_AffordableColor = Color.green;
         [SerializeField] private Color m_UnaffordableColor = Color.red;
+        [SerializeField] private Color m_SellColor = Color.yellow;
 
         public Color AffordableColor => m_AffordableColor;
         public Color UnaffordableColor => m_UnaffordableColor;
+        public Color SellColor => m_SellColor;
     }
 }
